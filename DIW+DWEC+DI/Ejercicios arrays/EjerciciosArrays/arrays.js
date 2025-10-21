f73a555628553ff3cf9de3cbf758b011b4354e41
Jesusdr32 <jesus.debenrodriguez@gmail.com>
2025-10-21 20:43:53 +0200

    modificaciones y .ova de SGE

--- DIW+DWEC+DI/Ejercicios arrays/EjerciciosArrays/arrays.js
+++ DIW+DWEC+DI/Ejercicios arrays/EjerciciosArrays/arrays.js
@@ -24,5 +24,44 @@ function Ejercicio4() {
 }
 
 function Ejercicio5() {
-    
+    let animales = ['perro', 'gato', 'elefante', 'jirafa'];
+    let animalesOrden = animales.sort((a, b) => {
+        if (a < b) return -1;
+        if (a > b) return 1;
+        return 0;
+    });
+    document.getElementById("arrayOrdenadoLetras").textContent = animalesOrden.join(", ");
+}
+
+function Ejercicio6() {
+    let fechas = ['2023-05-10', '2022-12-01', '2024-02-20'];
+    let fechasDate = fechas.map(a => new Date(a).toLocaleDateString('es-ES', {
+        year: 'numeric', 
+        month: '2-digit', 
+        day: '2-digit'}));
+    document.getElementById("arrayMapeadoDate").textContent = fechasDate.join('\n');
+}
+
+function Ejercicio7() {
+    let nums = [8, 15, 4, 20, 10];
+    let numsMayores = nums.filter(num => num > 10);
+    document.getElementById("arrayFiltradoMayor").textContent = numsMayores.join(", ");
+}
+
+function Ejercicio8() {
+    let objetos = [{nombre: 'Juan', edad: 25}, {nombre: 'Ana', edad: 30}, {nombre: 'Pedro', edad: 20}];
+    let objetosOrden = objetos.sort((a, b) => a.edad - b.edad);
+        document.getElementById("arrayOrdenadoEdad").textContent = objetosOrden.map(objeto => `${objeto.nombre} tiene ${objeto.edad} años`).join('\n');
+}
+
+function Ejercicio9() {
+    let saludos = ['hola', 'adiós', 'buenos días'];
+    let saludosMayusculas = saludos.map(a => a.toUpperCase());
+    document.getElementById("arrayMapeadoMayus").textContent = saludosMayusculas.join(", ");
+}
+
+function Ejercicio10() {
+    let libros = [{titulo: '1984', añoPublicacion:1949}, {titulo: 'Cien años de soledad', añoPublicacion: 1967}, {titulo: 'Matar a un ruiseñor', añoPublicacion: 1960}, {titulo: 'El gran Gatsby', añoPublicacion: 1925}];
+    let librosAños = libros.filter(a => a.añoPublicacion < 2000);
+    document.getElementById("arrayFiltradoLibros").textContent = librosAños.map(libro => `"${libro.titulo}" escrito en ${libro.añoPublicacion}`).join('\n');
 }
---
+++ PSP+PMDM/Unidad3/Practica MultiHilosII-BaileHowarts/src/paquete/Main.java
@@ -0,0 +1,11 @@
+package paquete;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DirectorBaile director = new DirectorBaile();
+		
+		Bailarin harry = new Bailarin("Harry Portter", "masculino" , "Gryffindor");
+	}
+
+}
